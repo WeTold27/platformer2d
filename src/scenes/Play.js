ae2b97d0b5e5eb0db7c36c14bbda1622bd7b8340
WeTold27 <lebedkin.n.v@mail.ru>
2025-07-27 17:59:11 +0500

    W

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,12 +7,37 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        const map = this.createMap();
+        const layers = this.createLayers(map);
+
+        const player = this.createPlayer();
+
+        this.physics.add.collider(player, layers.platformsColliders);
+
+    }
+
+    createMap() {
         const map = this.make.tilemap({key: 'map'});
-        const tileset1 = map.addTilesetImage('main_lev_build_1', 'tiles-1');
-        // const tileset2 = map.addTilesetImage('main_lev_build_2', 'tiles-2');
-        
-        map.createStaticLayer('environment', tileset1);
-        map.createStaticLayer('platforms', tileset1);
+        map.addTilesetImage('main_lev_build_1', 'tiles-1');
+        return map;
+    }
+
+    createLayers(map) {
+        const tileset = map.getTileset('main_lev_build_1');
+        const platformsColliders = map.createStaticLayer('platforms_colliders', tileset);
+        const environment = map.createStaticLayer('environment', tileset);
+        const platforms = map.createStaticLayer('platforms', tileset);
+
+        platformsColliders.setCollisionByProperty({collides: true});
+
+        return {environment, platforms, platformsColliders};
+    }
+
+    createPlayer() {
+        const player = this.physics.add.sprite(100, 250, 'player');
+        player.body.setGravityY(500);
+        player.setCollideWorldBounds(true);
+        return player;
     }
 }
 
